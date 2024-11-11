678614308a201aa452c1f0d404121941001f9c2c
Heloísa Loch <145377617+Helo270508@users.noreply.github.com>
2024-11-11 14:45:55 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'Meu trabalho é?',
+    'Estagiária'
+)
+
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    ' Quem sou eu?',
+    'Heloísa Loch Blau'
+)
+
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'O que eu mais gosto no meu trabalho é?',
+    'Organizar a secretaria'
+)
+
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'Nos finais de semana prefiro? ',
+    'Ficar com o meu namorado'
+)
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'Meu esporte favorito é? ',
+    'Futsal e Academia'
+)
